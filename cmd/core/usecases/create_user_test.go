7fec4f4570af44c9617df4ff6ceef123c84e59f6
riquemorozine <riquemorozinee@gmail.com>
2024-09-10 11:45:41 -0300

    feat: create user test

---
+++ cmd/core/usecases/create_user_test.go
@@ -0,0 +1,44 @@
+package usecases
+
+import (
+	"context"
+	"github.com/riquemorozine/todo_list_go/cmd/core/contracts"
+	"github.com/riquemorozine/todo_list_go/cmd/entities"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+
+	if err != nil {
+		t.Fatalf("could not set up test database: %v", err)
+	}
+
+	db.AutoMigrate(&entities.User{}, &entities.Todo{})
+
+	req := &contracts.CreateUserRequest{
+		Name:     "Test Name",
+		Email:    "test@email.com",
+		Password: "Test Password",
+	}
+
+	usecase := NewCreateUserUseCase(db)
+
+	response, err := usecase.Execute(context.Background(), req)
+
+	if err != nil {
+		t.Fatalf("could not create user: %v", err)
+	}
+
+	res, err := usecase.UserDB.FindByID(response.ID)
+	if err != nil {
+		t.Fatalf("could not find user: %v", err)
+	}
+
+	assert.NoError(t, err)
+	assert.Equal(t, req.Name, res.Name)
+	assert.Equal(t, req.Email, res.Email)
+}
