b6b37f049affc8811bb76861aaa40f2c1c67d6ce
riquemorozine <riquemorozinee@gmail.com>
2024-09-11 15:59:30 -0300

    feat: upate todo usecase

---
+++ cmd/core/usecases/todo/update_todo.go
@@ -0,0 +1,53 @@
+package todo
+
+import (
+	"context"
+	"github.com/riquemorozine/todo_list_go/cmd/core/contracts"
+	"github.com/riquemorozine/todo_list_go/cmd/infra/databases"
+	"gorm.io/gorm"
+)
+
+type UpdateTodoResponse struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+
+type UpdateTodoUseCase interface {
+	Execute(ctx context.Context, req *contracts.UpdateTodoRequest) (*UpdateTodoResponse, error)
+}
+
+func NewUpdateTodoUseCase(db *gorm.DB) UpdateTodoUseCaseImpl {
+	return UpdateTodoUseCaseImpl{
+		TodoDB: databases.NewTodo(db),
+	}
+}
+
+type UpdateTodoUseCaseImpl struct {
+	TodoDB *databases.Todo
+}
+
+func (imp *UpdateTodoUseCaseImpl) Execute(ctx context.Context, req *contracts.UpdateTodoRequest) (*UpdateTodoResponse, error) {
+	todo, err := imp.TodoDB.FindByID(req.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	todo.Title = req.Title
+	todo.Description = req.Description
+	todo.Status = req.Status
+	err = imp.TodoDB.Update(todo)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &UpdateTodoResponse{
+		ID:          todo.ID,
+		Title:       todo.Title,
+		Description: todo.Description,
+		Status:      todo.Status,
+	}, nil
+}
